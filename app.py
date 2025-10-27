5cd40b6cb752a315b9aeada87c9ae59342f08acd
louis-ker <louiskerguillec35@gmail.com>
2025-10-27 20:51:18 +0100

    adding images

--- app.py
+++ app.py
@@ -1,8 +1,39 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+from PIL import Image
+import base64
+from io import BytesIO
 
+st.set_page_config(layout="wide")
 st.title('Uber pickups in NYC')
+
+# Convertir l’image locale en base64
+def get_base64_of_image(image_path):
+    img = Image.open(image_path)
+    buffered = BytesIO()
+    img.save(buffered, format="JPEG")
+    return base64.b64encode(buffered.getvalue()).decode()
+
+img_base64 = get_base64_of_image("images/image.jpg")
+
+# Injecter l’image en HTML pleine largeur
+st.markdown(
+    f"""
+    <style>
+    .full-width-img {{
+        width: 100vw;
+        height: auto;
+        margin: 0;
+        padding: 0;
+        display: block;
+    }}
+    </style>
+    <img class="full-width-img" src="data:image/jpeg;base64,{img_base64}">
+    """,
+    unsafe_allow_html=True
+)
+
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
